59ef6cc5a345c7d58063530f18813a7fb34cbeb9
pedrofrxncx <pedrofrxncx@deco.cx>
2026-08-25 12:59:56 -0300

    Add workshop staging script

---
+++ sync-workshop.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Stage this mod into ~/Zomboid/Workshop for the in-game uploader.
+# ~/Zomboid/mods/HalfSmoked is the source of truth; run this before publishing.
+set -e
+SRC="$HOME/Zomboid/mods/HalfSmoked"
+DST="$HOME/Zomboid/Workshop/HalfSmoked/Contents/mods/HalfSmoked"
+mkdir -p "$DST"
+rsync -a --delete \
+  --exclude='.git' --exclude='.gitignore' --exclude='.DS_Store' \
+  --exclude='README.md' --exclude='sync-workshop.sh' \
+  "$SRC/" "$DST/"
+echo "synced -> $DST"
